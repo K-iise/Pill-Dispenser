28f14ff34e936e3369627a14daa192dc11cf20f1
K-iise <kimyungi1004@naver.com>
2024-05-26 22:20:24 +0900

    알람 조회 UI / 미완

---
+++ app/src/main/java/com/capstone/pilldispenser/Alarm_select.java
@@ -0,0 +1,36 @@
+package com.capstone.pilldispenser;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class Alarm_select extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_select);
+
+        // Menu Button, Drawer 생성
+        ImageButton menuButton = (ImageButton) findViewById(R.id.action_ham);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+
+        // 메뉴바 클릭 이벤트(Drawer 출력)
+        menuButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (!drawer.isDrawerOpen(Gravity.LEFT)) {
+                    drawer.openDrawer(Gravity.LEFT) ;
+                }
+            }
+        });
+    }
+}
