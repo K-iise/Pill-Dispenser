68717573d408dcb852b847423c6038067f4aeeea
K-iise <kimyungi1004@naver.com>
2024-05-29 23:37:51 +0900

    회원가입, 아이디/비밀번호 찾기 UI

---
+++ app/src/main/java/com/capstone/pilldispenser/IDfindUI.java
@@ -0,0 +1,19 @@
+package com.capstone.pilldispenser;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IDfindUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idfind_ui);
+
+    }
+}
